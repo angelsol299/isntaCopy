e584b3b63441985ea972213cfaba1b794732bef4
angelsol299 <angelsol299@hotmail.com>
2018-11-19 20:03:45 +0100

    google auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
+import firebase from "firebase";
 
 class App extends Component {
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>insta</h2>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <p className="App-intro">
             <button onClick={this.handleAuth}>Login con Google</button>
           </p>

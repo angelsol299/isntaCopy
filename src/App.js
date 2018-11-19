e86010bd98a60d26b5297462259842e7c1e0ade8
angelsol299 <angelsol299@hotmail.com>
2018-11-19 20:15:27 +0100

    adding login functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import "./App.css";
 import firebase from "firebase";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+  }
+
   handleAuth() {
     const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -11,16 +18,33 @@ class App extends Component {
       .auth()
       .signInWithPopup(provider)
       .then(result => console.log(`${result.user.email} has started a session`))
-      .catch();
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
+
+  renderLoginButton() {
+    //si esta logueado
+    if (this.state.user) {
+      return (
+        <div>
+          <img
+            src="this.state.user.photoURL"
+            alt="{this.state.user.displayName}"
+          />
+          <p>hola {this.state.user.displayName}</p>
+        </div>
+      );
+    } else {
+      //si no esta logueado
+      return <button onClick={this.handleAuth}>Login con Google</button>;
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h2>insta</h2>
-          <p className="App-intro">
-            <button onClick={this.handleAuth}>Login con Google</button>
-          </p>
+          <p className="App-intro">{this.renderLoginButton}</p>
         </header>
       </div>
     );
